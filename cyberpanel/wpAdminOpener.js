6c215af165dca5d8ebc184ef229cce42e1c477b3
davidgb-intratum <davidgb.intratum@gmail.com>
2025-07-29 11:46:01 +0200

    Update wpAdminOpener.js

--- cyberpanel/wpAdminOpener.js
+++ cyberpanel/wpAdminOpener.js
@@ -1,12 +1,32 @@
-(function() {
-  const urlParams = new URLSearchParams(window.location.search);
-  let domain = urlParams.get('domain');
+(function () {
+    const pathname = window.location.pathname;
 
-  if (!domain) {
-    domain = prompt("Enter domain:");
-  }
+    if (pathname !== "/wp-login.php") {
+        const urlParams = new URLSearchParams(window.location.search);
+        let domain = urlParams.get('domain');
 
-  if (domain) {
-    window.location.href = `https://${domain}/wp-admin/plugin-install.php`;
-  }
+        if (!domain) {
+            domain = prompt("Enter domain:");
+        }
+
+        if (domain) {
+            window.location.href = `https://${domain}/wp-admin/plugin-install.php`;
+        }
+
+        return;
+    }
+
+    const userInput = document.getElementById("user_login");
+    const passInput = document.getElementById("user_pass");
+    const submitBtn = document.getElementById("wp-submit");
+
+    if (userInput && passInput && submitBtn) {
+        userInput.value = "intratum";
+        passInput.value = "intratum";
+
+        userInput.dispatchEvent(new Event("input", { bubbles: true }));
+        passInput.dispatchEvent(new Event("input", { bubbles: true }));
+
+        setTimeout(() => submitBtn.click(), 100);
+    }
 })();
