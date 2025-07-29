7cf2f071ec21d994ce8c627e68a14b2825a89fba
davidgb-intratum <davidgb.intratum@gmail.com>
2025-07-29 10:15:34 +0200

    Create createUser.js

---
+++ cyberpanel/createUser.js
@@ -0,0 +1,57 @@
+if (window.location.pathname !== "/users/createUser") {
+    window.location.href = "/users/createUser";
+    return;
+}
+
+const domainsInput = prompt("Enter domain(s), comma-separated:");
+const userPass = prompt("Enter user password:");
+
+const domains = domainsInput
+    .split(',')
+    .map(d => d.trim())
+    .filter(d => d.length);
+
+const form = document.querySelector("form[name=createUser]");
+
+function fillAndSubmit(domain, delay = 0) {
+    setTimeout(() => {
+        const $form = angular.element(form);
+        const $scope = $form.scope();
+
+        const data = {
+            firstName: (domain => (domain.split('.').slice(-2, -1)[0] || '').slice(0, 32))(domain),
+            lastName: "intratum",
+            email: `www@${domain}`,
+            websitesLimits: 1,
+            userName: domain,
+            password: userPass,
+            selectedACL: "user",
+            securityLevel: "HIGH"
+        };
+
+        Object.entries(data).forEach(([model, value]) => {
+            const input = form.querySelector(`[ng-model='${model}']`);
+            if (input) {
+                const $input = angular.element(input);
+                const ctrl = $input.controller('ngModel');
+                if (ctrl) {
+                    ctrl.$setViewValue(value);
+                    ctrl.$render();
+                }
+            }
+        });
+
+        $scope.$apply();
+
+        form.querySelector("button[ng-click='createUserFunc()']").click();
+    }, delay);
+}
+
+domains.forEach((domain, i) => {
+    fillAndSubmit(domain, i * 1500);
+});
+
+setTimeout(() => {
+    alert("All users created!")
+    window.location.href = "/websites/createWebsite";
+}, domains.length * 1500 + 1000);
