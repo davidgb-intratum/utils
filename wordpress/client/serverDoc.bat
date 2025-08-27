eebf01886ed90abd5c598b010aefd9d399c0bb80
davidgb-intratum <davidgb.intratum@gmail.com>
2025-08-27 17:25:55 +0200

    Delete wordpress/client/serverDoc.bat

--- wordpress/client/serverDoc.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set /p SERVER_IP=Enter the server IP: 
-
-ssh -t USER@%SERVER_IP% "curl -fsSL -o /tmp/serverDoc.sh https://raw.githubusercontent.com/davidgb-intratum/utils/refs/heads/main/wordpress/server/serverDoc.sh && chmod +x /tmp/serverDoc.sh && sudo /tmp/serverDoc.sh"
-
-PAUSE
